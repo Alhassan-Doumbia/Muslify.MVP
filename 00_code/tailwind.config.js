e1bb4f78c7b3e1fa22ff4266c9561a804e2b6234
Doumbia Alhassan <hassan4cod@gmail.com>
2024-03-18 09:25:41 +0000

    importation de nouvelles dependances + installation de Tailwindcss

---
+++ 00_code/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],// represente les fichiers dans lesquels il doit rechercher les différentes classes Tailwindcss
+  //pour appliquer Tailwindcss
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
